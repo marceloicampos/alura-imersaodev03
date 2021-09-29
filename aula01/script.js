088f744a40ab09a2cfd37f032cfe306deb1cc637
marceloicampos <marcelo.ildefonso@hotmail.com>
2021-09-29 00:12:54 -0300

    aula01-final

--- aula01/script.js
+++ aula01/script.js
@@ -1,20 +1,34 @@
-var userName = 'marceloicampos'
-// strings ficam entres aspas
+// var userName = 'marceloicampos'
+// // strings ficam entres aspas
 
-var firstNote = 8.991
-var secondNote = 9.891
-var thirdNote = 9.891
-var fourthNote = 10.891
-var finalNote = ((firstNote + secondNote + thirdNote + fourthNote) / 4).toFixed(
-    1
-)
-// numbers NÃO ficam entres aspas
+// var firstNote = 8.991
+// var secondNote = 9.891
+// var thirdNote = 9.891
+// var fourthNote = 10.891
+// var finalNote = ((firstNote + secondNote + thirdNote + fourthNote) / 4).toFixed(1)
+// // numbers NÃO ficam entres aspas
 
-console.log('Bem vindo', userName)
+// console.log('Bem vindo', userName)
 
-console.log('Primeira Nota', firstNote)
-console.log('Segunda Nota', secondNote)
-console.log('Terceira Nota', thirdNote)
-console.log('Quarta Nota', fourthNote)
+// console.log('Primeira Nota', firstNote)
+// console.log('Segunda Nota', secondNote)
+// console.log('Terceira Nota', thirdNote)
+// console.log('Quarta Nota', fourthNote)
 
-console.log('Média das Nota', finalNote)
+// console.log('Média das Nota', finalNote)
+
+function Nota() {
+    var nota1 = parseFloat(document.getElementById('nota1').value)
+    // console.log(nota1)
+
+    var nota2 = parseFloat(document.getElementById('nota2').value)
+    // console.log(nota2)
+
+    var media = ((nota1 + nota2) / 2).toFixed(1)
+
+    var mostraMedia = 'Sua Média Escolar é: ' + media
+
+    var elementMedia = document.getElementById('notaMedia')
+
+    elementMedia.innerHTML = mostraMedia
+}
